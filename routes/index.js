812540872f53c0e64558e767126890fa6ee59261
Neon Engfer <nengfer@gmail.com>
2014-04-15 13:04:02 -0700

    Crude login logic implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var baseTitle = 'CaptionIt';
+var connections = {};
+var isLoggedIn = false;
 
 exports.index = function(req, res, next) {
-  res.render('index', {title: baseTitle});
+  if (connections[req.ip] === undefined) {
+    res.redirect('/login');
+  }
+  
+  res.render('index', {title: baseTitle, isLoggedIn: isLoggedIn});
 }
 
 exports.login = function(req, res, next) {
-  res.render('login', {title: baseTitle + ' - Login'});
+  if (connections[req.ip] === undefined) {
+    res.render('login', {title: baseTitle + ' - Login', isLoggedIn: isLoggedIn});
+  } else {
+    res.redirect('/');
+  }
+}
+
+exports.authenticate = function(req, res, next) {
+  if (req.body.name === undefined) {
+    res.redirect('/login');
+  } else {
+    connections[req.ip] = { name: req.body.name };
+    isLoggedIn = true;
+    
+    res.redirect('/');
+  }
 }
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var routes = require('./routes');
 
 app.configure(function() {
+  // app.use(express.json());
+  // app.use(express.urlencoded());
+  app.use(express.bodyParser());
 	app.engine('html', require('ejs').__express);	
 	app.set('view engine', 'html');
   app.set('views', __dirname + '/app/views');
@@ -13,6 +16,7 @@ app.configure(function() {
 // Define routing
 app.get('/', routes.index);
 app.get('/login', routes.login);
+app.post('/authenticate', routes.authenticate);
 
 var server = app.listen(1234, function() {
 	console.log('server running on localhost:1234');
